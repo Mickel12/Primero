3ec4b3767cd5a5dce312f3be503072040604c08c
Mickel Loza <mickel_loza@hotmail.com>
2021-01-21 10:26:19 -0500

    21/01/21 Mi primera version

---
+++ js/formulario.js
@@ -0,0 +1,19 @@
+$(document).on("click", "#btn_registrar", function () {
+    //validar apellido
+    let apellido = $("#txtapellidos").val();
+    if (apellido.trim().legth==0) {
+        alert("debe eligir un apellido");
+        return false;
+
+    }
+    let nombre = $("#txtnombre").val();
+    if (nombre.trim().legth==0) {
+        alert("debe eligir un nombre");
+        return false;
+
+    }
+
+    alert("datos correctos");
+})
+
+
